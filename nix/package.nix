003e41df70548fee3d647e3367968e2c32b8a057
Jörg Thalheim <joerg@thalheim.io>
2026-04-30 00:26:05 +0200

    fix nixfmt tests

--- nix/package.nix
+++ nix/package.nix
@@ -1,7 +1,11 @@
-{ rustPlatform }:
+{ rustPlatform, nixfmt }:
 rustPlatform.buildRustPackage {
   pname = "nixfmt-rs";
   version = "0.1.0";
   src = ../.;
   cargoLock.lockFile = ../Cargo.lock;
+  # The test suite shells out to the reference Haskell `nixfmt` to compare
+  # output, so it must be on PATH during checkPhase.
+  nativeCheckInputs = [ nixfmt ];
 }
+
